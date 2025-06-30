d2f1ae189c45645fdcbf49ff004eaae9ede37e99
dhichiafis <91750915+dhichiafis@users.noreply.github.com>
2025-06-30 21:20:22 +0200

    third trial

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ app.include_router(user_router)
 @app.get('/')
 async def home():
     return {'home':'this is the home page'}
+
+
+if __name__=="__main__":
+    uvicorn.run("main:app",reload=True,host='127.0.0.1',port=8000)
